c4b860c461199279d95fe0ebf67306f2374fde9a
aryakumar95 <55797028+aryakumar95@users.noreply.github.com>
2023-08-04 10:37:01 +0530

    Update main.tf

--- K8SService/main.tf
+++ K8SService/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     node_count          = var.system_node_count
     vm_size             = "Standard_B2s"
     type                = "VirtualMachineScaleSets"
-    // availability_zones  = [1, 2, 3]
+    // availability_zones = [1, 2, 3]
      enable_auto_scaling = true
     max_count = 3
     min_count = 1
